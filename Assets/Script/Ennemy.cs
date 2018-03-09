265703cb16614d8f38bfc30007308695543eaaa3
Zeldarck <zeldarck.martin@gmail.com>
2018-03-09 23:12:21 +0100

    Revert "Revert "Revert "Debug Loic"""
    This reverts commit 7cc65db260d7f6697db9c787e52cb52f4f006cd4.

--- Assets/Script/Ennemy.cs
+++ Assets/Script/Ennemy.cs
@@ -5,15 +5,13 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class EnemyBehavior : MonoBehaviour {
+public class EnnemyBehavior : MonoBehaviour {
 
-    protected static int _POS_X = 0; // TO SET : Initial Z position of the enemy
-    protected static int _POS_Y = 0; // TO SET : Initial Y position of the enemy
-    protected static int _DEADZONE = 0; // TO SET : Lower limit for which the game ends if an invader come through
+    protected static int _POS_X = 0; // TO SET
+    protected static int _POS_Y = 0; // TO SET
 
-    protected float _enemyRadius;
-    protected float _XReverse = -1;
-    protected float _YForward = 0;
+    protected Rigidbody _enemyRB;
+    protected float _direction;
 
     [SerializeField]
     protected float _enemySpeed;
@@ -23,30 +21,32 @@ public class EnemyBehavior : MonoBehaviour {
 
     public UnityEvent OnDie; // TO CHECK
 
+
     // Use this for initialization
     protected void Start()
     {
-        
-        // Set the initial position of the enemy     
+        _enemyRB = gameObject.GetComponent<Rigidbody>();
         gameObject.transform.position = new Vector2(_POS_X, _POS_Y);
-        
+
+
+        _direction = 1;
+
+
         OnDie.AddListener(() => StartCoroutine(AutoDestroy())); // TO CHECK
-                
+
+        
     }
     
     // On trigger get damages from any damage source
     public bool GetDamage(int a_damage)
     {
-
         m_life -= a_damage;
         if (m_life <= 0)
         {
             OnDie.Invoke(); // TO CHECK
             return true;
         }
-
         return false;
-
     }
 
     
@@ -58,47 +58,34 @@ public class EnemyBehavior : MonoBehaviour {
         Destroy(gameObject);
     }
 
-
-    
     // In case of collision with the border walls, change the direction of the ennemy
-    void OnCollisionEnter2D(Collision2D other)
+    protected void OnTriggerEnter(Collider other)
     {
-        if (other.collider.CompareTag("Wall"))
+        if (other.gameObject.CompareTag("Wall"))
         {
-            _XReverse *= -1;
-            _YForward = -1;
+            // TO DO 
         }
     }
     
-
-    protected Vector2 DirectionComputation()
-    {
-        Vector2 directionComputed = new Vector2(_XReverse, _YForward);
-
-        directionComputed *= _enemySpeed;
-
-        Debug.Log(directionComputed);
-        return directionComputed;
-    }
-
-
     // Position update which is called once per frame
     void FixedUpdate()
     {
-        
-        // check if an invader has made it to the Dead Zone
-        if (gameObject.transform.position.y < (_DEADZONE + _enemyRadius))
+        /*
+        //check if Y < 0.45 (0.9 = ennemy's height)
+        if (gameObject.transform.position.y < (_MIN_Y + 0.8))
         {
             Debug.Log("An Enemy reached the bottom!");
 
-            // GameController.EndGame(); // TO LINK OR IMPLEMENT LATER
+            GameController.EndGame();
             Destroy(gameObject);
         }
-                
+        */
+
+
         // move accordingly 
-        Vector2 direction = DirectionComputation();
-        Debug.Log(direction);
-        gameObject.transform.position = (Vector2) gameObject.transform.position + direction * Time.deltaTime;
+        // To change and adapt with velocity
+        Vector3 directionToGo = new Vector3(0, 0, 0);
+        _enemyRB.position += directionToGo * Time.deltaTime;
 
     }
 
