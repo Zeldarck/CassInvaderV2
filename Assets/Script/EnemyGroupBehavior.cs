083df0c5f22acfbcfd82a75ef91c2603d0c788aa
Ecyanaro <shurtugal_777@live.fr>
2018-03-30 22:13:49 +0200

    EnemyManager
    Update with an Enemy manager which invokes ennemies regulary

--- Assets/Script/EnemyGroupBehavior.cs
+++ Assets/Script/EnemyGroupBehavior.cs
@@ -5,20 +5,52 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
-public class EnemyBehavior : Ennemies
+public class EnemyGroupBehavior : Ennemies
 {
- 
+    [SerializeField]
+    Ennemies m_EnemyPrefab;
+
+    protected int enemyCount;
+    protected int enemyStartCount;
+
+    /// <summary>
+    /// Later use to count the number of ennemies in the group
+    /// </summary>
+    override protected void Awake()
+    {
+        enemyStartCount = 1;
+    }
+
+    /// <summary>
+    /// Add the enemy child in the group and link its transform to the group
+    /// </summary>
+    public void AddChild(Ennemies child)
+    {
+        child.transform.SetParent(this.transform);
+    }
+
+    /// <summary>
+    /// In this override, we count the number of ennemies in the group once
+    /// </summary>
+    override protected void Start()
+    {
+
+        // IF enemyStartCount = 1 : 
+        // enemyCount = this.transform.childCount; 
+        // else : nothing
+
+    }
 
     // Compute the direction vector of the enemy depending of the current situation
     override protected Vector2 DirectionComputation()
     {
-        
         // Check if the invader is too close to the edge and change its direction accordingly
         if (gameObject.transform.position.x >= (_INIT_POS_X + _X_MOVE_EAST) || gameObject.transform.position.x <= (_INIT_POS_X + _X_MOVE_WEST))
         {
             _XDir *= -1;
             _YDir = -10;
         }
+        // Use the number of ennmies in the wave to move everybody accordingly
 
         Vector2 directionComputed = new Vector2(_XDir, _YDir);
         directionComputed *= _enemySpeed;
@@ -27,13 +59,12 @@ public class EnemyBehavior : Ennemies
         _YDir = 0;
 
         return directionComputed;
-        
     }
 
-    // Update the position of the enemy
+    // Update the position of the enemy group
     override protected void FixedUpdate()
     {
-        /*
+
         // check if an invader has made it to the Dead Zone
         if (gameObject.transform.position.y < (_DEADZONE + _enemyRadius))
         {
@@ -47,6 +78,6 @@ public class EnemyBehavior : Ennemies
         Vector2 direction = DirectionComputation();
         Debug.Log(direction);
         gameObject.transform.position = (Vector2)gameObject.transform.position + direction * Time.deltaTime;
-        */
+
     }
 }
---
+++ Assets/Script/EnemyManager.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class EnemyManager : MonoBehaviour
+{
+
+    public static EnemyManager INSTANCE;
+    
+    [SerializeField]
+    GameObject EnemyGroupPrefab;
+
+    [SerializeField]
+    Ennemies m_EnemyPrefab;
+
+    /// <summary>
+    /// Path to the level designs file and associated attributes
+    /// </summary>
+    private string gameDataProjectFilePath = "/Assets/Levels/level_1.txt";
+
+    private float spawnTime = 25f;            // How long between each spawn.
+    private int _nbEnemyToSpawn = 6;
+    private int _nbWavesEnemys = 6;
+
+    private Vector3 initialPosition = new Vector3(0, 5, 0);
+    private Vector3 enemyPosition = new Vector3(0, 5, 0);
+    private Quaternion initialRotation = new Quaternion(0, 0, 0, 0);
+
+    void Start()
+    {
+
+        if (INSTANCE != null && INSTANCE != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            INSTANCE = this;
+        }
+        
+        // Call the Spawn function after a delay of the spawnTime and then continue to call after the same amount of time.
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    void Spawn()
+    {
+        /// <summary>
+        /// If the game has ended
+        /// </summary>
+        /*
+        if (CONDITION)
+        {
+            // ... exit the function.
+            return;
+        }
+        */
+
+        /// <summary>
+        /// Instantiate a new group of ennemies
+        /// </summary>
+        GameObject EnemyGroup = Instantiate(EnemyGroupPrefab, initialPosition, initialRotation);
+
+        for(float i = 0; i < _nbEnemyToSpawn; ++i)
+        {
+            Debug.Log(i);
+            enemyPosition = new Vector3(((i*1.2f)/2 - (_nbEnemyToSpawn*1.2f)/4), 5 , 0);
+            EnemyGroup.GetComponent<EnemyGroupBehavior>().AddChild(Instantiate(m_EnemyPrefab, enemyPosition, initialRotation));
+        }
+            
+    }
+
+
+    void loadGameData()
+    {
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    /*
+    public GameData gameData;
+
+    
+
+    [MenuItem("Window/Game Data Editor")]
+    static void Init()
+    {
+        EditorWindow.GetWindow(typeof(GameDataEditor)).Show();
+    }
+
+    private void LoadGameData()
+    {
+        string filePath = Application.dataPath + gameDataProjectFilePath;
+
+        if (File.Exists(filePath))
+        {
+            string dataAsJson = File.ReadAllText(filePath);
+            gameData = JsonUtility.FromJson<GameData>(dataAsJson);
+        }
+        else
+        {
+            gameData = new GameData();
+        }
+    }
+
+
+
+    */
+
+
+
+
+
+
+
+}
