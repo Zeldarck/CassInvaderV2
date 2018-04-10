e1677e1efe5d054528a2a543c99ed98450f4e8d8
Zeldarck <zeldarck.martin@gmail.com>
2018-04-10 22:27:52 +0200

    delete pong - add game manager

--- Assets/Script/Utils.cs
+++ Assets/Script/Utils.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Utils {
 
+    static System.Random m_random = new System.Random();
+
     public static void DestroyChilds(Transform a_transform)
     {
         for (int i = a_transform.childCount - 1; i >= 0; --i)
@@ -12,15 +14,24 @@ public class Utils {
         }
     }
 
-    public static bool RandomBool(System.Random r, int probability)
+    public static bool RandomBool( int probability, System.Random r = null)
     {
+        if (r == null)
+        {
+            r = m_random;
+        }
+
         int v = r.Next(0, 100);
         bool result = v < probability;
         return result;
     }
 
-    public static float RandomFloat(System.Random r, float min, float max)
+    public static float RandomFloat(float min, float max, System.Random r = null)
     {
+        if(r == null)
+        {
+            r = m_random;
+        }
         return (float)r.NextDouble() * (max - min) + min;
     }
 
@@ -28,5 +39,21 @@ public class Utils {
     {
         return a_value >= -a_epsilon && a_value <= a_epsilon ? 0 : (int)Mathf.Sign(a_value);
     }
+
+    public static float NextGaussianDouble(float a_mean, float a_stdDev)
+    {
+        float u, v, S;
+        
+        do
+        {
+            u = 2.0f * Random.value - 1.0f;
+            v = 2.0f * Random.value - 1.0f;
+            S = u * u + v * v;
+        } while (S >= 1.0f || S == 0f);
+
+        float fac = Mathf.Sqrt((-2.0f * Mathf.Log(S) / S));
+        return a_mean + a_stdDev * u * fac;
+    }
+
 
 }
