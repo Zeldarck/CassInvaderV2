d3f74b5a6a76d9be4075adfd5ab76b64feab8813
Zeldarck <zeldarck.martin@gmail.com>
2018-04-10 22:27:58 +0200

    same

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager INSTANCE;
+    /// <summary>
+    /// Score of player 1
+    /// </summary>
+    static int m_playerScore = 0;
+
+
+    public static int PlayerScore
+    {
+        get
+        {
+            return m_playerScore;
+        }
+    }
+
+    void Start()
+    {
+        if (INSTANCE != null && INSTANCE != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            INSTANCE = this;
+        }
+    }
+
+    void Update()
+    {
+    }
+
+
+}
